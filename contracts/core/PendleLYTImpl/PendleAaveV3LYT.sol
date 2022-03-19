69e042743c4bc87032aef81c614288ad59ccc2f3
Minh Nguyễn Hoàng Hải <minhhhn@Minhs-MacBook-Pro.local>
2022-03-19 08:33:01 +0800

    Add Aave LYT

---
+++ contracts/core/PendleLYTImpl/PendleAaveV3LYT.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+import "../../LiquidYieldToken/implementations/LYTBaseWithRewards.sol";
+import "../../interfaces/IAToken.sol";
+import "../../interfaces/IAavePool.sol";
+import "../../interfaces/IAaveRewardsController.sol";
+import "../../libraries/math/WadRayMath.sol";
+
+contract PendleAaveV3LYT is LYTBaseWithRewards {
+    using WadRayMath for uint256;
+    using SafeERC20 for IERC20;
+
+    address internal immutable underlying;
+    address internal immutable pool;
+    address internal immutable rewardsController;
+    address internal immutable aToken;
+
+    uint256 internal lastLytIndex;
+
+    // WIP: Aave reward controller can config to have more rewardsToken,
+    // hence rewardsLength should not be immutable
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 __lytdecimals,
+        uint8 __assetDecimals,
+        address _aavePool,
+        address _underlying,
+        address _aToken,
+        address _rewardsController
+    ) LYTBaseWithRewards(_name, _symbol, __lytdecimals, __assetDecimals) {
+        aToken = _aToken;
+        pool = _aavePool;
+        underlying = _underlying;
+        rewardsController = _rewardsController;
+        IERC20(underlying).safeIncreaseAllowance(aToken, type(uint256).max);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                    DEPOSIT/REDEEM USING BASE TOKENS
+    //////////////////////////////////////////////////////////////*/
+
+    function _deposit(address token, uint256 amountBase)
+        internal
+        virtual
+        override
+        returns (uint256 amountLytOut)
+    {
+        // aTokenScaled -> lyt is 1:1
+        if (token == aToken) {
+            amountLytOut = amountBase.rayDiv(lytIndexCurrent());
+        } else {
+            IAavePool(pool).supply(underlying, amountBase, address(this), 0);
+            _afterSendToken(underlying);
+            amountLytOut = _afterReceiveToken(aToken);
+        }
+    }
+
+    function _redeem(address token, uint256 amountLyt)
+        internal
+        virtual
+        override
+        returns (uint256 amountBaseOut)
+    {
+        if (token == aToken) {
+            amountBaseOut = amountLyt.rayMul(lytIndexCurrent());
+        } else {
+            uint256 amountBaseExpected = amountLyt.rayMul(lytIndexCurrent());
+            IAavePool(pool).withdraw(underlying, amountBaseExpected, address(this));
+            _afterSendToken(aToken);
+            amountBaseOut = _afterReceiveToken(token);
+        }
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                               LYT-INDEX
+    //////////////////////////////////////////////////////////////*/
+
+    function lytIndexCurrent() public virtual override returns (uint256 res) {
+        res = IAavePool(pool).getReserveNormalizedIncome(underlying);
+        lastLytIndex = res;
+        return res;
+    }
+
+    function lytIndexStored() public view override returns (uint256 res) {
+        res = lastLytIndex;
+    }
+
+    function getRewardTokens() public view override returns (address[] memory res) {
+        return IAaveRewardsController(rewardsController).getRewardsByAsset(aToken);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                MISC FUNCTIONS FOR METADATA
+    //////////////////////////////////////////////////////////////*/
+
+    function getBaseTokens() public view virtual override returns (address[] memory res) {
+        res = new address[](2);
+        res[0] = underlying;
+        res[1] = aToken;
+    }
+
+    function isValidBaseToken(address token) public view virtual override returns (bool res) {
+        res = (token == underlying || token == aToken);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                               REWARDS-RELATED
+    //////////////////////////////////////////////////////////////*/
+
+    function _redeemExternalReward() internal override {
+        address[] memory assets = new address[](1);
+        assets[0] = aToken;
+
+        IAaveRewardsController(rewardsController).claimAllRewards(assets, address(this));
+    }
+
+    /// @dev balance of aToken is saved by scaledBalanceOf instead
+    function _afterReceiveToken(address token) internal virtual override returns (uint256 res) {
+        if (token == aToken) {
+            uint256 curBalance = IAToken(aToken).scaledBalanceOf(address(this));
+            res = (curBalance - lastBalanceOf[token]).rayMul(lytIndexCurrent());
+            lastBalanceOf[token] = curBalance;
+        } else {
+            uint256 curBalance = IERC20(token).balanceOf(address(this));
+            res = curBalance - lastBalanceOf[token];
+            lastBalanceOf[token] = curBalance;
+        }
+    }
+
+    function _afterSendToken(address token) internal virtual override {
+        if (token == aToken) {
+            lastBalanceOf[token] = IAToken(aToken).scaledBalanceOf(address(this));
+        } else {
+            lastBalanceOf[token] = IERC20(token).balanceOf(address(this));
+        }
+    }
+}
---
+++ contracts/interfaces/IAToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * MIT License
+ * ===========
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
+pragma solidity ^0.8.0;
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+interface IAToken is IERC20 {
+    /**
+     * @dev Returns the scaled balance of the user. The scaled balance is the sum of all the
+     * updated stored balance divided by the reserve's liquidity index at the moment of the update
+     * @param user The user whose balance is calculated
+     * @return The scaled balance of the user
+     **/
+    function scaledBalanceOf(address user) external view returns (uint256);
+}
