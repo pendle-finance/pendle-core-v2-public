b67911fceef2d7837b28f24af6bb22c18b3d3d03
ngfam <ngfamgithub@gmail.com>
2022-04-11 09:47:30 +0700

    Fix absolute path

--- contracts/core/router/PendleRouterProxy.sol
+++ contracts/core/router/PendleRouterProxy.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "contracts/core/misc/BoringOwnableUpg.sol";
+import "../misc/BoringOwnableUpg.sol";
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "../../interfaces/IPRouterCore.sol";
 import "../../interfaces/IPRouterYT.sol";
