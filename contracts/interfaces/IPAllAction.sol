70d0887896037a4569852efa358123a312112750
UncleGrandpa925 <vuonglongtoan@gmail.com>
2022-04-18 22:31:24 +0800

    Add IPAllAction

---
+++ contracts/interfaces/IPAllAction.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.9;
+
+import "./IPActionCore.sol";
+import "./IPActionYT.sol";
+import "./IPActionStatic.sol";
+
+interface IPAllAction is IPActionCore, IPActionYT, IPActionStatic {}
