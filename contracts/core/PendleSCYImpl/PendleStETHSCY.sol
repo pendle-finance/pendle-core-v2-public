c9485e0510938106174912cee4ced05f0d7707c9
UncleGrandpa925 <vuonglongtoan@gmail.com>
2022-04-21 10:35:37 +0800

    Pin solidity version of StETHSCY

--- contracts/core/PendleSCYImpl/PendleStETHSCY.sol
+++ contracts/core/PendleSCYImpl/PendleStETHSCY.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
-pragma abicoder v2;
+pragma solidity 0.8.9;
 import "../../SuperComposableYield/implementations/SCYBase.sol";
 import "../../interfaces/IWstETH.sol";
 
