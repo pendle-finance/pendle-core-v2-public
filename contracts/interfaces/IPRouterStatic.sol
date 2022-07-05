69d353ea28903f094845c46de9a5808ceb4ac58b
Jin Heng Yap <jinhengyap@gmail.com>
2022-07-05 11:38:31 +0800

    [RF] Change assetBalance type for SCY amounts

--- contracts/interfaces/IPRouterStatic.sol
+++ contracts/interfaces/IPRouterStatic.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.13;
 
+import "./ISuperComposableYield.sol";
 import "../libraries/math/MarketMathCore.sol";
 
 interface IPRouterStatic {
     struct TokenAmount {
         address token;
+        uint256 amount;
+    }
+
+    struct AssetAmount {
+        ISuperComposableYield.AssetType assetType;
+        address assetAddress;
         uint256 amount;
     }
 
@@ -28,8 +35,56 @@ interface IPRouterStatic {
         uint256 lpBalance;
         TokenAmount ptBalance;
         TokenAmount scyBalance;
-        TokenAmount assetBalance;
+        AssetAmount assetBalance;
     }
+
+    // ============= SYSTEM INFO =============
+
+    function getPYInfo(address py)
+        external
+        returns (
+            uint256 exchangeRate,
+            uint256 totalSupply,
+            RewardIndex[] memory rewardIndexes
+        );
+
+    function getPendleTokenType(address token)
+        external
+        view
+        returns (
+            bool isPT,
+            bool isYT,
+            bool isMarket
+        );
+
+    function getMarketInfo(address market)
+        external
+        view
+        returns (
+            address pt,
+            address scy,
+            MarketState memory state,
+            int256 impliedYield
+        );
+
+    // ============= USER INFO =============
+
+    function getUserPYPositionsByPYs(address user, address[] calldata pys)
+        external
+        view
+        returns (UserPYInfo[] memory userPYPositions);
+
+    function getUserMarketPositions(address user, address[] calldata markets)
+        external
+        view
+        returns (UserMarketInfo[] memory userMarketPositions);
+
+    function getUserSCYInfo(address scy, address user)
+        external
+        view
+        returns (uint256 balance, TokenAmount[] memory rewards);
+
+    // ============= MARKET ACTIONS =============
 
     function addLiquidityStatic(
         address market,
@@ -37,6 +92,7 @@ interface IPRouterStatic {
         uint256 ptDesired
     )
         external
+        view
         returns (
             uint256 netLpOut,
             uint256 scyUsed,
@@ -50,70 +106,34 @@ interface IPRouterStatic {
 
     function swapPtForScyStatic(address market, uint256 exactPtIn)
         external
+        view
         returns (uint256 netScyOut, uint256 netScyFee);
 
     function swapScyForPtStatic(address market, uint256 exactPtOut)
         external
+        view
         returns (uint256 netScyIn, uint256 netScyFee);
 
-    function scyIndex(address market) external returns (SCYIndex index);
+    // ============= OTHER HELPERS =============
+
+    function scyIndex(address market) external view returns (SCYIndex index);
+
+    function getPY(address py) external view returns (address pt, address yt);
 
     function getPtImpliedYield(address market) external view returns (int256);
-
-    function getPendleTokenType(address token)
-        external
-        view
-        returns (
-            bool isPT,
-            bool isYT,
-            bool isMarket
-        );
 
     function getUserPYInfo(address py, address user)
         external
         view
         returns (UserPYInfo memory userPYInfo);
 
-    function getPYInfo(address py)
-        external
-        returns (
-            uint256 exchangeRate,
-            uint256 totalSupply,
-            RewardIndex[] memory rewardIndexes
-        );
-
-    function getPY(address py) external view returns (address yt, address pt);
-
-    function getMarketInfo(address market)
-        external
-        view
-        returns (
-            address pt,
-            address scy,
-            MarketState memory state,
-            int256 impliedYield,
-            uint256 exchangeRate
-        );
-
     function getUserMarketInfo(address market, address user)
         external
         view
         returns (UserMarketInfo memory userMarketInfo);
 
-    function getUserPYPositionsByPYs(address user, address[] calldata pys)
+    function hasPYPosition(UserPYInfo calldata userPYInfo)
         external
-        view
-        returns (UserPYInfo[] memory userPYPositions);
-
-    function getUserMarketPositions(address user, address[] calldata markets)
-        external
-        view
-        returns (UserMarketInfo[] memory userMarketPositions);
-
-    function hasPYPosition(UserPYInfo memory userPYInfo) external pure returns (bool hasPosition);
-
-    function getUserSCYInfo(address scy, address user)
-        external
-        view
-        returns (uint256 balance, TokenAmount[] memory rewards);
+        pure
+        returns (bool hasPosition);
 }
