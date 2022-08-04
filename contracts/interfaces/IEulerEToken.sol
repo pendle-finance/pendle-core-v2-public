239c2fe6776cc2dbdb937530fdab37518448977f
victortzy <106247981+victortzy@users.noreply.github.com>
2022-08-04 23:41:37 +0800

    Euler Finance SCY (#103)
    Co-authored-by: victortanzy123 <83770436+victortanzy123@users.noreply.github.com>
    Co-authored-by: UncleGrandpa925 <vuonglongtoan@gmail.com>

---
+++ contracts/interfaces/IEulerEToken.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * MIT License
+ * ===========
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ */
+
+pragma solidity 0.8.15;
+
+interface IEulerEToken {
+    /// @notice Address of underlying asset
+    function underlyingAsset() external view returns (address);
+
+    /// @notice Sum of all balances, in internal book-keeping units (non-increasing)
+    function totalSupply() external view returns (uint256);
+
+    /// @notice Sum of all balances, in underlying units (increases as interest is earned)
+    function totalSupplyUnderlying() external view returns (uint256);
+
+    /// @notice Transfer underlying tokens from sender to the Euler pool, and increase account's eTokens
+    /// @param subAccountId 0 for primary, 1-255 for a sub-account
+    /// @param amount In underlying units (use max uint256 for full underlying token balance)
+    function deposit(uint256 subAccountId, uint256 amount) external;
+
+    function balanceOf(address) external view returns (uint256);
+
+    /// @notice Balance of a particular account, in underlying units (increases as interest is earned)
+    function balanceOfUnderlying(address) external view returns (uint256);
+
+    /// @notice Convert an underlying amount to an eToken balance, taking into account current exchange rate
+    /// @param underlyingAmount Amount in underlying units (same decimals as underlying token)
+    /// @return eToken balance, in internal book-keeping units (18 decimals)
+    function convertUnderlyingToBalance(uint256 underlyingAmount) external view returns (uint256);
+
+    /// @notice Convert an eToken balance to an underlying amount, taking into account current exchange rate
+    /// @param balance eToken balance, in internal book-keeping units (18 decimals)
+    /// @return Amount in underlying units, (same decimals as underlying token)
+    function convertBalanceToUnderlying(uint256 balance) external view returns (uint256);
+
+    /// @notice Transfer underlying tokens from Euler pool to sender, and decrease account's eTokens
+    /// @param subAccountId 0 for primary, 1-255 for a sub-account
+    /// @param amount In underlying units (use max uint256 for full pool balance)
+    function withdraw(uint256 subAccountId, uint256 amount) external;
+}
