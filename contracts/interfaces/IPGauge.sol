7b4c5a30238caccd328e69afe454231ed6512c46
ngfam <ngfamgithub@gmail.com>
2022-08-31 08:45:03 +0700

    [SF] Add activeBalance to IPGauge interface

--- contracts/interfaces/IPGauge.sol
+++ contracts/interfaces/IPGauge.sol
@@ -3,4 +3,6 @@ pragma solidity 0.8.15;
 
 interface IPGauge {
     function totalActiveSupply() external view returns (uint256);
+
+    function activeBalance(address user) external view returns (uint256);
 }
