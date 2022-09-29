metatrust-v/pendle-core-v2

[RF] Refactor entire codebase to use CustomError

contracts/core/Market/PendleMarket.sol
@@ -59,7 +59,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
     OracleLib.Observation[65535] public observations;
 
     modifier notExpired() {
-        require(!isExpired(), "market expired");
+        if (isExpired()) revert Errors.MarketExpired();
         _;
     }
 
@@ -80,7 +80,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         (_storage.observationCardinality, _storage.observationCardinalityNext) = observations
             .initialize(uint32(block.timestamp));
 
-        if (_scalarRoot <= 0) revert Errors.MarketScalarRootTooLow(_scalarRoot);
+        if (_scalarRoot <= 0) revert Errors.MarketScalarRootBelowZero(_scalarRoot);
 
         scalarRoot = _scalarRoot;
         initialAnchor = _initialAnchor;
@@ -128,9 +128,9 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         _writeState(market);
 
         if (_selfBalance(SCY) < market.totalScy.Uint())
-            revert Errors.MarketInsufficientSCY(_selfBalance(SCY), market.totalScy.Uint());
+            revert Errors.MarketInsufficientScyBal(_selfBalance(SCY), market.totalScy.Uint());
         if (_selfBalance(PT) < market.totalPt.Uint())
-            revert Errors.MarketInsufficientPT(_selfBalance(PT), market.totalPt.Uint());
+            revert Errors.MarketInsufficientPtBal(_selfBalance(PT), market.totalPt.Uint());
 
         emit Mint(receiver, netLpOut, netScyUsed, netPtUsed);
     }
@@ -191,7 +191,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         }
 
         if (_selfBalance(PT) < market.totalPt.Uint())
-            revert Errors.MarketInsufficientPT(_selfBalance(PT), market.totalPt.Uint());
+            revert Errors.MarketInsufficientPtBal(_selfBalance(PT), market.totalPt.Uint());
 
         emit Swap(receiver, exactPtIn.neg(), netScyOut.Int(), netScyToReserve);
     }
@@ -229,7 +229,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
 
         // have received enough SCY
         if (_selfBalance(SCY) < market.totalScy.Uint())
-            revert Errors.MarketInsufficientSCY(_selfBalance(SCY), market.totalScy.Uint());
+            revert Errors.MarketInsufficientScyBal(_selfBalance(SCY), market.totalScy.Uint());
 
         emit Swap(receiver, exactPtOut.Int(), netScyIn.neg(), netScyToReserve);
     }

contracts/libraries/Errors.sol
@@ -4,19 +4,115 @@ pragma solidity 0.8.15;
 /// Adapted from UniswapV3's Oracle
 
 library Errors {
+    // MARKET + MARKET MATH CORE
     error MarketExpired();
     error MarketZeroAmountsInput();
     error MarketZeroAmountsOutput();
     error MarketZeroLnImpliedRate();
-    error MarketInsufficientLiquidity();
-
-    error MarketInsufficientPT(uint256 actualBalance, uint256 requiredBalance);
-    error MarketInsufficientSCY(uint256 actualBalance, uint256 requiredBalance);
-
-    error MarketInvalidState(int256 totalPt, int256 totalAsset);
+    error MarketInsufficientPtForTrade(int256 currentAmount, int256 requiredAmount);
+    error MarketInsufficientPtBal(uint256 actualBalance, uint256 requiredBalance);
+    error MarketInsufficientScyBal(uint256 actualBalance, uint256 requiredBalance);
+    error MarketZeroTotalPtOrTotalAsset(int256 totalPt, int256 totalAsset);
     error MarketExchangeRateBelowOne(int256 exchangeRate);
     error MarketProportionMustNotEqualOne();
-    error MarketProportionTooHigh(int256 proportion);
-    error MarketRateScalarTooLow(int256 rateScalar);
-    error MarketScalarRootTooLow(int256 scalarRoot);
+    error MarketRateScalarBelowZero(int256 rateScalar);
+    error MarketScalarRootBelowZero(int256 scalarRoot);
+    error MarketProportionTooHigh(int256 proportion, int256 maxProportion);
+
+    error OracleUninitialized();
+    error OracleTargetTooOld(uint32 target, uint32 oldest);
+    error OracleZeroCardinality();
+
+    error MarketFactoryExpiredPt();
+    error MarketFactoryInvalidPt();
+    error MarketFactoryMarketExists();
+
+    error MFactoryLnFeeRateRootTooHigh(uint88 lnFeeRateRoot, uint256 maxLnFeeRateRoot);
+    error MFactoryReserveFeePercentTooHigh(uint8 reserveFeePercent, uint8 maxReserveFeePercent);
+
+    // ROUTER
+    error RouterInsufficientLpOut(uint256 actualLpOut, uint256 requiredLpOut);
+    error RouterInsufficientScyOut(uint256 actualScyOut, uint256 requiredScyOut);
+    error RouterInsufficientPtOut(uint256 actualPtOut, uint256 requiredPtOut);
+    error RouterInsufficientYtOut(uint256 actualYtOut, uint256 requiredYtOut);
+    error RouterInsufficientPYOut(uint256 actualPYOut, uint256 requiredPYOut);
+    error RouterInsufficientTokenOut(uint256 actualTokenOut, uint256 requiredTokenOut);
+    error RouterExceededLimitScyIn(uint256 actualScyIn, uint256 limitScyIn);
+    error RouterExceededLimitPtIn(uint256 actualPtIn, uint256 limitPtIn);
+    error RouterExceededLimitYtIn(uint256 actualYtIn, uint256 limitYtIn);
+    error RouterInsufficientScyRepay(uint256 actualScyRepay, uint256 requiredScyRepay);
+    error RouterInsufficientPtRepay(uint256 actualPtRepay, uint256 requiredPtRepay);
+
+    error RouterTimeRangeZero();
+    error RouterCallbackNotPendleMarket(address caller);
+    error RouterInvalidAction(bytes4 selector);
+
+    // YIELD CONTRACT
+    error YCExpired();
+    error YCNotExpired();
+    error YieldContractInsufficientScy(uint256 actualScy, uint256 requiredScy);
+    error YCNothingToRedeem();
+    error YCPostExpiryDataNotSet();
+    error YCNoFloatingScy();
+
+    // YieldFactory
+    error YCFactoryInvalidExpiry();
+    error YCFactoryYieldContractExisted();
+
+    // SCY
+    error SCYInvalidTokenIn(address token);
+    error SCYInvalidTokenOut(address token);
+    error SCYZeroDeposit();
+    error SCYZeroRedeem();
+    error SCYInsufficientSharesOut(uint256 actualSharesOut, uint256 requiredSharesOut);
+    error SCYInsufficientTokenOut(uint256 actualTokenOut, uint256 requiredTokenOut);
+
+    // SCY-specific
+    error SCYQiTokenMintFailed(uint256 errCode);
+    error SCYQiTokenRedeemFailed(uint256 errCode);
+    error SCYQiTokenRedeemRewardsFailed(uint256 rewardAccuredType0, uint256 rewardAccuredType1);
+    error SCYQiTokenBorrowRateTooHigh(uint256 borrowRate, uint256 borrowRateMax);
+
+    // Liquidity Mining
+    error VCInactivePool(address pool);
+    error VCPoolAlreadyActive(address pool);
+    error VCZeroVePendle(address user);
+    error VCExceededMaxWeight(uint256 totalWeight, uint256 maxWeight);
+    error VCEpochNotFinalized(uint256 wTime);
+    error VCPoolAlreadyAddAndRemoved(address pool);
+
+    error VEInvalidNewExpiry(uint256 newExpiry);
+    error VEExceededMaxLockTime();
+    error VEInsufficientLockTime();
+    error VENotAllowedReduceExpiry();
+    error VEZeroAmountLocked();
+    error VEPositionNotExpired();
+    error VEZeroPosition();
+    error VEZeroSlope(uint128 bias, uint128 slope);
+
+    error GCNotPendleMarket(address caller);
+    error GCNotVotingController(address caller);
+
+    error InvalidWTime(uint256 wTime);
+    error ExpiryInThePast(uint256 expiry);
+    error ChainNotSupported(uint256 chainId);
+
+    error FDCantFundFutureEpoch();
+    error FDFactoryDistributorAlreadyExisted(address pool, address distributor);
+
+    // Cross-Chain
+    error MsgNotFromSendEndpoint(address sender);
+    error MsgNotFromReceiveEndpoint(address sender);
+    error InsufficientFeeToSendMsg(uint256 currentFee, uint256 requiredFee);
+
+    // GENERIC MSG
+    error ArrayLengthMismatch();
+    error ArrayEmpty();
+    error ArrayOutOfBounds();
+    error ZeroAddress();
+
+    error OnlyCelerBus();
+    error OnlyYT();
+    error OnlyYCFactory();
+    error OnlyWhitelisted();
 }

pendle-finance/pendle-core-v2-public

contracts/core/Market/PendleMarket.sol
@@ -59,7 +59,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
     OracleLib.Observation[65535] public observations;
 
     modifier notExpired() {
-        require(!isExpired(), "market expired");
+        if (isExpired()) revert Errors.MarketExpired();
         _;
     }
 
@@ -80,7 +80,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         (_storage.observationCardinality, _storage.observationCardinalityNext) = observations
             .initialize(uint32(block.timestamp));
 
-        if (_scalarRoot <= 0) revert Errors.MarketScalarRootTooLow(_scalarRoot);
+        if (_scalarRoot <= 0) revert Errors.MarketScalarRootBelowZero(_scalarRoot);
 
         scalarRoot = _scalarRoot;
         initialAnchor = _initialAnchor;
@@ -128,9 +128,9 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         _writeState(market);
 
         if (_selfBalance(SCY) < market.totalScy.Uint())
-            revert Errors.MarketInsufficientSCY(_selfBalance(SCY), market.totalScy.Uint());
+            revert Errors.MarketInsufficientScyReceived(_selfBalance(SCY), market.totalScy.Uint());
         if (_selfBalance(PT) < market.totalPt.Uint())
-            revert Errors.MarketInsufficientPT(_selfBalance(PT), market.totalPt.Uint());
+            revert Errors.MarketInsufficientPtReceived(_selfBalance(PT), market.totalPt.Uint());
 
         emit Mint(receiver, netLpOut, netScyUsed, netPtUsed);
     }
@@ -191,7 +191,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
         }
 
         if (_selfBalance(PT) < market.totalPt.Uint())
-            revert Errors.MarketInsufficientPT(_selfBalance(PT), market.totalPt.Uint());
+            revert Errors.MarketInsufficientPtReceived(_selfBalance(PT), market.totalPt.Uint());
 
         emit Swap(receiver, exactPtIn.neg(), netScyOut.Int(), netScyToReserve);
     }
@@ -229,7 +229,7 @@ contract PendleMarket is PendleERC20Permit, PendleGauge, IPMarket {
 
         // have received enough SCY
         if (_selfBalance(SCY) < market.totalScy.Uint())
-            revert Errors.MarketInsufficientSCY(_selfBalance(SCY), market.totalScy.Uint());
+            revert Errors.MarketInsufficientScyReceived(_selfBalance(SCY), market.totalScy.Uint());
 
         emit Swap(receiver, exactPtOut.Int(), netScyIn.neg(), netScyToReserve);
     }

contracts/libraries/Errors.sol
@@ -4,19 +4,118 @@ pragma solidity 0.8.15;
 /// Adapted from UniswapV3's Oracle
 
 library Errors {
+    // MARKET + MARKET MATH CORE
     error MarketExpired();
     error MarketZeroAmountsInput();
     error MarketZeroAmountsOutput();
     error MarketZeroLnImpliedRate();
-    error MarketInsufficientLiquidity();
-
-    error MarketInsufficientPT(uint256 actualBalance, uint256 requiredBalance);
-    error MarketInsufficientSCY(uint256 actualBalance, uint256 requiredBalance);
-
-    error MarketInvalidState(int256 totalPt, int256 totalAsset);
+    error MarketInsufficientPtForTrade(int256 currentAmount, int256 requiredAmount);
+    error MarketInsufficientPtReceived(uint256 actualBalance, uint256 requiredBalance);
+    error MarketInsufficientScyReceived(uint256 actualBalance, uint256 requiredBalance);
+    error MarketZeroTotalPtOrTotalAsset(int256 totalPt, int256 totalAsset);
     error MarketExchangeRateBelowOne(int256 exchangeRate);
     error MarketProportionMustNotEqualOne();
-    error MarketProportionTooHigh(int256 proportion);
-    error MarketRateScalarTooLow(int256 rateScalar);
-    error MarketScalarRootTooLow(int256 scalarRoot);
+    error MarketRateScalarBelowZero(int256 rateScalar);
+    error MarketScalarRootBelowZero(int256 scalarRoot);
+    error MarketProportionTooHigh(int256 proportion, int256 maxProportion);
+
+    error OracleUninitialized();
+    error OracleTargetTooOld(uint32 target, uint32 oldest);
+    error OracleZeroCardinality();
+
+    error MarketFactoryExpiredPt();
+    error MarketFactoryInvalidPt();
+    error MarketFactoryMarketExists();
+
+    error MFactoryLnFeeRateRootTooHigh(uint88 lnFeeRateRoot, uint256 maxLnFeeRateRoot);
+    error MFactoryReserveFeePercentTooHigh(uint8 reserveFeePercent, uint8 maxReserveFeePercent);
+
+    // ROUTER
+    error RouterInsufficientLpOut(uint256 actualLpOut, uint256 requiredLpOut);
+    error RouterInsufficientScyOut(uint256 actualScyOut, uint256 requiredScyOut);
+    error RouterInsufficientPtOut(uint256 actualPtOut, uint256 requiredPtOut);
+    error RouterInsufficientYtOut(uint256 actualYtOut, uint256 requiredYtOut);
+    error RouterInsufficientPYOut(uint256 actualPYOut, uint256 requiredPYOut);
+    error RouterInsufficientTokenOut(uint256 actualTokenOut, uint256 requiredTokenOut);
+    error RouterExceededLimitScyIn(uint256 actualScyIn, uint256 limitScyIn);
+    error RouterExceededLimitPtIn(uint256 actualPtIn, uint256 limitPtIn);
+    error RouterExceededLimitYtIn(uint256 actualYtIn, uint256 limitYtIn);
+    error RouterInsufficientScyRepay(uint256 actualScyRepay, uint256 requiredScyRepay);
+    error RouterInsufficientPtRepay(uint256 actualPtRepay, uint256 requiredPtRepay);
+
+    error RouterTimeRangeZero();
+    error RouterCallbackNotPendleMarket(address caller);
+    error RouterInvalidAction(bytes4 selector);
+
+    // YIELD CONTRACT
+    error YCExpired();
+    error YCNotExpired();
+    error YieldContractInsufficientScy(uint256 actualScy, uint256 requiredScy);
+    error YCNothingToRedeem();
+    error YCPostExpiryDataNotSet();
+    error YCNoFloatingScy();
+
+    // YieldFactory
+    error YCFactoryInvalidExpiry();
+    error YCFactoryYieldContractExisted();
+
+    // SCY
+    error SCYInvalidTokenIn(address token);
+    error SCYInvalidTokenOut(address token);
+    error SCYZeroDeposit();
+    error SCYZeroRedeem();
+    error SCYInsufficientSharesOut(uint256 actualSharesOut, uint256 requiredSharesOut);
+    error SCYInsufficientTokenOut(uint256 actualTokenOut, uint256 requiredTokenOut);
+
+    // SCY-specific
+    error SCYQiTokenMintFailed(uint256 errCode);
+    error SCYQiTokenRedeemFailed(uint256 errCode);
+    error SCYQiTokenRedeemRewardsFailed(uint256 rewardAccuredType0, uint256 rewardAccuredType1);
+    error SCYQiTokenBorrowRateTooHigh(uint256 borrowRate, uint256 borrowRateMax);
+
+    error SCYCurveInvalidPid();
+    error SCYCurve3crvPoolNotFound();
+
+    // Liquidity Mining
+    error VCInactivePool(address pool);
+    error VCPoolAlreadyActive(address pool);
+    error VCZeroVePendle(address user);
+    error VCExceededMaxWeight(uint256 totalWeight, uint256 maxWeight);
+    error VCEpochNotFinalized(uint256 wTime);
+    error VCPoolAlreadyAddAndRemoved(address pool);
+
+    error VEInvalidNewExpiry(uint256 newExpiry);
+    error VEExceededMaxLockTime();
+    error VEInsufficientLockTime();
+    error VENotAllowedReduceExpiry();
+    error VEZeroAmountLocked();
+    error VEPositionNotExpired();
+    error VEZeroPosition();
+    error VEZeroSlope(uint128 bias, uint128 slope);
+
+    error GCNotPendleMarket(address caller);
+    error GCNotVotingController(address caller);
+
+    error InvalidWTime(uint256 wTime);
+    error ExpiryInThePast(uint256 expiry);
+    error ChainNotSupported(uint256 chainId);
+
+    error FDCantFundFutureEpoch();
+    error FDFactoryDistributorAlreadyExisted(address pool, address distributor);
+
+    // Cross-Chain
+    error MsgNotFromSendEndpoint(address sender);
+    error MsgNotFromReceiveEndpoint(address sender);
+    error InsufficientFeeToSendMsg(uint256 currentFee, uint256 requiredFee);
+
+    // GENERIC MSG
+    error ArrayLengthMismatch();
+    error ArrayEmpty();
+    error ArrayOutOfBounds();
+    error ZeroAddress();
+
+    error OnlyCelerBus();
+    error OnlyYT();
+    error OnlyYCFactory();
+    error OnlyWhitelisted();
 }