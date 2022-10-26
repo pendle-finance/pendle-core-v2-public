e9644906ff140667116acef92a322bb4e40a61f9
NXTung <nxtung@pendle.finance>
2022-10-26 12:31:56 +0700

    [DOCS] Reformat docs

--- contracts/interfaces/IPYieldContractFactory.sol
+++ contracts/interfaces/IPYieldContractFactory.sol
@@ -39,29 +39,45 @@ interface IPYieldContractFactory {
     /// @dev emitted when a treasury address is set
     event SetTreasury(address indexed treasury);
 
-    /// @dev returns (if created) the PT address for the corresponding (`SY`) token and (`expiry`)
-    /// @dev if the PY pair has not been created, returns `address(0)` instead
+    /**
+     * @notice returns the PT address for the corresponding (`SY`) token and (`expiry`), if created
+     * @dev if the corresponding PY pair has not been created, returns `address(0)` instead
+     */ 
     function getPT(address SY, uint256 expiry) external view returns (address);
 
-    /// @dev returns (if created) the YT address for the corresponding (`SY`) token and (`expiry`)
-    /// @dev if the PY pair has not been created, returns `address(0)` instead
+    /**
+     * @notice returns the YT address for the corresponding (`SY`) token and (`expiry`), if created
+     * @dev if the corresponding PY pair has not been created, returns `address(0)` instead
+     */
     function getYT(address SY, uint256 expiry) external view returns (address);
 
-    /// @dev returns the current expiry divisor
+    /**
+     * @notice returns the current expiry divisor
+     */
     function expiryDivisor() external view returns (uint96);
 
-    /// @dev returns the current interest fee rate
+    /**
+     * @notice returns the current interest fee rate
+     */
     function interestFeeRate() external view returns (uint128);
 
-    /// @dev returns the current reward fee rate
+    /**
+     * @notice returns the current reward fee rate
+     */
     function rewardFeeRate() external view returns (uint128);
 
-    /// @dev returns the current treasury address
+    /**
+     * @notice returns the current treasury address
+     */
     function treasury() external view returns (address);
 
-    /// @dev checks if an address is a created PT token
+    /**
+     * @notice checks if an address is a created PT token
+     */
     function isPT(address) external view returns (bool);
 
-    /// @dev checks if an address is a created YT token
+    /**
+     * @notice checks if an address is a created YT token
+     */ 
     function isYT(address) external view returns (bool);
 }
