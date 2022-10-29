76108772c396d29cd2049c881448cebf8cc50903
UncleGrandpa925 <vuonglongtoan@gmail.com>
2022-10-30 00:44:58 +0800

    [RF] Edit Curve3CrvPoolHelper to be more similar to the original code

--- contracts/core/StandardizedYield/implementations/Convex/base/Curve3CrvPoolHelper.sol
+++ contracts/core/StandardizedYield/implementations/Convex/base/Curve3CrvPoolHelper.sol
@@ -18,59 +18,7 @@ library Curve3CrvPoolHelper {
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-    function preview3CrvDeposit(address token, uint256 amount)
-        internal
-        view
-        returns (uint256 netLpOut, uint256 new_virtual_price)
-    {
-        uint256[N_COINS] memory _amounts = _getTokenAmounts(token, amount);
-
-        uint256 amp = ITriCrvPool(POOL).A();
-        uint256 _admin_fee = ITriCrvPool(POOL).admin_fee();
-
-        uint256[N_COINS] memory old_balances = _getBalances();
-        uint256[N_COINS] memory new_balances;
-        uint256[N_COINS] memory balances_after;
-        memcpy(new_balances, old_balances);
-        memcpy(balances_after, old_balances);
-
-        uint256 D0 = _get_D_mem(old_balances, amp);
-        uint256 total_supply = IERC20(LP).totalSupply();
-
-        for (uint256 i = 0; i < N_COINS; ++i) {
-            // skip totalSupply = 0 check
-            new_balances[i] += _amounts[i];
-        }
-
-        uint256 D1 = _get_D_mem(new_balances, amp);
-        assert(D1 > D0);
-
-        uint256[N_COINS] memory fees;
-
-        // skip total_supply > 0 check
-        uint256 fee = (ITriCrvPool(POOL).fee() * N_COINS) / (4 * (N_COINS - 1));
-        for (uint256 i = 0; i < N_COINS; ++i) {
-            uint256 ideal_balance = (D1 * old_balances[i]) / D0;
-            uint256 difference = 0;
-            uint256 new_balance = new_balances[i];
-
-            if (ideal_balance > new_balance) {
-                difference = ideal_balance - new_balance;
-            } else {
-                difference = new_balance - ideal_balance;
-            }
-            fees[i] = (fee * difference) / FEE_DENOMINATOR;
-            balances_after[i] = new_balances[i] - ((fees[i] * _admin_fee) / FEE_DENOMINATOR);
-            new_balances[i] -= fees[i];
-        }
-        uint256 D2 = _get_D_mem(new_balances, amp);
-
-        netLpOut = (total_supply * (D2 - D0)) / D0;
-        new_virtual_price = _get_virtual_price_with_balances(
-            balances_after,
-            total_supply + netLpOut
-        );
-    }
+    // previewDeposit at the end of the file
 
     function preview3CrvRedeem(address tokenOut, uint256 amountSharesToRedeem)
         internal
@@ -127,47 +75,101 @@ library Curve3CrvPoolHelper {
         return (token == DAI || token == USDC || token == USDT);
     }
 
-    function memcpy(uint256[N_COINS] memory a, uint256[N_COINS] memory b) internal pure {
-        for (uint256 i = 0; i < N_COINS; ++i) {
-            a[i] = b[i];
-        }
-    }
-
     function get_virtual_price() internal view returns (uint256) {
         return ITriCrvPool(POOL).get_virtual_price();
     }
 
-    function _get_virtual_price_with_balances(
-        uint256[N_COINS] memory balances,
-        uint256 new_totalSupply
-    ) internal view returns (uint256) {
+    // ----------------- Forking StableSwap3Pool's calculation -----------------
+
+    // fork of StableSwap3Pool.vy
+    function preview3CrvDeposit(address token, uint256 amount)
+        internal
+        view
+        returns (uint256 netLpOut, uint256 new_virtual_price)
+    {
+        uint256[N_COINS] memory amounts = _getTokenAmounts(token, amount);
+        uint256[N_COINS] memory self_balances = _getBalances();
+
+        uint256[N_COINS] memory fees;
+        uint256 _fee = (ITriCrvPool(POOL).fee() * N_COINS) / (4 * (N_COINS - 1));
+        uint256 _admin_fee = ITriCrvPool(POOL).admin_fee();
         uint256 amp = ITriCrvPool(POOL).A();
-        uint256 D = _get_D_mem(balances, amp);
-        return (D * PRECISION) / new_totalSupply;
+
+        uint256 token_supply = IERC20(LP).totalSupply();
+        // Initial invariant
+        uint256 D0 = 0;
+        uint256[N_COINS] memory old_balances = self_balances;
+        D0 = get_D_mem(old_balances, amp);
+
+        uint256[N_COINS] memory new_balances = old_balances;
+
+        for (uint256 i = 0; i < N_COINS; ++i) {
+            new_balances[i] += amounts[i];
+        }
+
+        uint256 D1 = get_D_mem(new_balances, amp);
+        assert(D1 > D0);
+
+        // We need to recalculate the invariant accounting for fees
+        // to calculate fair user's share
+        uint256 D2 = D1;
+        for (uint256 i = 0; i < N_COINS; ++i) {
+            uint256 ideal_balance = (D1 * old_balances[i]) / D0;
+            uint256 difference = 0;
+            if (ideal_balance > new_balances[i]) {
+                difference = ideal_balance - new_balances[i];
+            } else {
+                difference = new_balances[i] - ideal_balance;
+            }
+            fees[i] = (_fee * difference) / FEE_DENOMINATOR;
+            self_balances[i] = new_balances[i] - ((fees[i] * _admin_fee) / FEE_DENOMINATOR);
+            new_balances[i] -= fees[i];
+            D2 = get_D_mem(new_balances, amp);
+        }
+
+        netLpOut = (token_supply * (D2 - D0)) / D0;
+        new_virtual_price = _get_virtual_price_with_balances(
+            self_balances,
+            token_supply + netLpOut
+        );
     }
 
-    function _get_D_mem(uint256[N_COINS] memory balances, uint256 _amp)
+    function _get_virtual_price_with_balances(
+        uint256[N_COINS] memory balances,
+        uint256 token_supply
+    ) internal view returns (uint256) {
+        uint256 amp = ITriCrvPool(POOL).A();
+        uint256 D = get_D_mem(balances, amp);
+        return (D * PRECISION) / token_supply;
+    }
+
+    function get_D_mem(uint256[N_COINS] memory balances, uint256 amp)
         internal
         pure
         returns (uint256)
     {
-        uint256[N_COINS] memory _xp;
-        _xp[0] = (RATE_0 * balances[0]) / PRECISION;
-        _xp[1] = (RATE_1 * balances[1]) / PRECISION;
-        _xp[2] = (RATE_2 * balances[2]) / PRECISION;
-
-        return _get_D(_xp, _amp);
+        return get_D(_xp_mem(balances), amp);
     }
 
-    function _get_D(uint256[N_COINS] memory _xp, uint256 _amp) internal pure returns (uint256) {
-        uint256 S = 0;
-        uint256 Dprev = 0;
+    function _xp_mem(uint256[N_COINS] memory balances)
+        internal
+        pure
+        returns (uint256[N_COINS] memory)
+    {
+        uint256[N_COINS] memory result;
+        result[0] = (RATE_0 * balances[0]) / PRECISION;
+        result[1] = (RATE_1 * balances[1]) / PRECISION;
+        result[2] = (RATE_2 * balances[2]) / PRECISION;
 
-        for (uint256 k = 0; k < N_COINS; ++k) {
-            S += _xp[k];
-        }
+        return result;
+    }
+
+    function get_D(uint256[N_COINS] memory _xp, uint256 _amp) internal pure returns (uint256) {
+        uint256 S = 0;
+        for (uint256 k = 0; k < N_COINS; ++k) S += _xp[k];
         if (S == 0) return 0;
 
+        uint256 Dprev = 0;
         uint256 D = S;
         uint256 Ann = _amp * N_COINS;
 
@@ -177,19 +179,19 @@ library Curve3CrvPoolHelper {
                 D_P = (D_P * D) / (_xp[k] * N_COINS);
             }
             Dprev = D;
-            D = (((Ann * S) + D_P * N_COINS) * D) / (((Ann - 1) * D) + (N_COINS + 1) * D_P);
+            D = ((Ann * S + D_P * N_COINS) * D) / (((Ann - 1) * D) + (N_COINS + 1) * D_P);
 
             if (D > Dprev) {
                 if (D - Dprev <= 1) {
-                    return D;
+                    break;
                 }
             } else {
                 if (Dprev - D <= 1) {
-                    return D;
+                    break;
                 }
             }
         }
-        assert(false);
+        return D;
     }
 
     function _getBalances() internal view returns (uint256[N_COINS] memory balances) {
