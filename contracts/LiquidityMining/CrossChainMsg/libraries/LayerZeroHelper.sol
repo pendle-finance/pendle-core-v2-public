db8bf7fa82f14413e91b87b95a5b47cff5f5f8de
ngfam <ngfamgithub@gmail.com>
2023-01-30 09:46:57 +0700

    [MF] Added LZ helper support for Arbitrum One

--- contracts/LiquidityMining/CrossChainMsg/libraries/LayerZeroHelper.sol
+++ contracts/LiquidityMining/CrossChainMsg/libraries/LayerZeroHelper.sol
@@ -5,20 +5,18 @@ library LayerZeroHelper {
     uint256 constant EVM_ADDRESS_SIZE = 20;
 
     function _getLayerZeroChainIds(uint256 chainId) internal pure returns (uint16) {
-        if (chainId == 43113) return 10106;
-        // fuji testnet
-        else if (chainId == 80001) return 10109;
-        // mumbai testnet
-        else if (chainId == 43114) return 106; // avax mainnet
+        if (chainId == 43113) return 10106; // fuji testnet
+        else if (chainId == 80001) return 10109; // mumbai testnet
+        else if (chainId == 43114) return 106;  // avax mainnet
+        else if (chainId == 42161) return 110; // arbitrum one
         assert(false);
     }
 
     function _getOriginalChainIds(uint16 chainId) internal pure returns (uint256) {
-        if (chainId == 10106) return 43113;
-        // fuji testnet
-        else if (chainId == 10109) return 80001;
-        // mumbai testnet
+        if (chainId == 10106) return 43113; // fuji testnet
+        else if (chainId == 10109) return 80001; // mumbai testnet
         else if (chainId == 106) return 43114; // avax mainnet
+        else if (chainId == 110) return 42161; // arbitrum one
         assert(false);
     }
 
