131aa7b6467875005c88f3f54e5447bc1d1b9243
ngfam <ngfamgithub@gmail.com>
2023-03-03 13:13:31 +0700

    [RF] Add necessary interface for testing

--- contracts/interfaces/GMX/IGlpManager.sol
+++ contracts/interfaces/GMX/IGlpManager.sol
@@ -5,4 +5,8 @@ interface IGlpManager {
     function getAumInUsdg(bool maximise) external view returns (uint256);
 
     function vault() external view returns (address);
+
+    function gov() external view returns (address);
+
+    function setCooldownDuration(uint256 _cooldownDuration) external;
 }
