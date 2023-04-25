3987dca2794ce526b0f213a688d66e209695467b
UncleGrandpa925 <vuonglongtoan@gmail.com>
2023-04-25 10:04:50 +0700

    [SF] Add EmptyUUPS

---
+++ contracts/offchain-helpers/EmptyUUPS.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "../core/libraries/BoringOwnableUpgradeable.sol";
+
+contract EmptyUUPS is Initializable, UUPSUpgradeable, BoringOwnableUpgradeable {
+    constructor() initializer {}
+
+    function initialize() external initializer {
+        __BoringOwnable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
