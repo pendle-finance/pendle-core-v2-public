8d9bf198a842e348189347496aec0ee5e52f550c
ngfam <ngfamgithub@gmail.com>
2023-07-05 11:58:21 +0700

    [SF] AnkrBNB implementation

---
+++ contracts/interfaces/Ankr/IAnkrBNB.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.17;
+
+interface IAnkrBNB {
+    function ratio() external view returns (uint256);
+
+    function bondsToShares(uint256 amount) external view returns (uint256);
+
+    function sharesToBonds(uint256 amount) external view returns (uint256);
+}
