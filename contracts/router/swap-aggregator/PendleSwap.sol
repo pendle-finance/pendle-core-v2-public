5362ccb79f7208f307550caf49e01537be71216b
UncleGrandpa925 <vuonglongtoan@gmail.com>
2023-07-06 23:58:39 +0800

    [MF] Add support for 1inch

--- contracts/router/swap-aggregator/PendleSwap.sol
+++ contracts/router/swap-aggregator/PendleSwap.sol
@@ -4,8 +4,9 @@ pragma solidity 0.8.17;
 import "../../core/libraries/TokenHelper.sol";
 import "./IPSwapAggregator.sol";
 import "./kyberswap/KyberInputScalingHelper.sol";
+import "./1inch/OneInchAggregationRouterHelper.sol";
 
-contract PendleSwap is IPSwapAggregator, TokenHelper, KyberInputScalingHelper {
+contract PendleSwap is IPSwapAggregator, TokenHelper, KyberInputScalingHelper, OneInchAggregationRouterHelper {
     using Address for address;
 
     function swap(
@@ -30,7 +31,7 @@ contract PendleSwap is IPSwapAggregator, TokenHelper, KyberInputScalingHelper {
         if (swapType == SwapType.KYBERSWAP) {
             scaledCallData = _getKyberScaledInputData(rawCallData, amountIn);
         } else if (swapType == SwapType.ONE_INCH) {
-            revert("not supported");
+            scaledCallData = _get1inchScaledInputData(rawCallData, amountIn);
         } else {
             assert(false);
         }
