1c90c162193139b496e59eab51a901b1cf310bbc
UncleGrandpa925 <vuonglongtoan@gmail.com>
2023-08-14 13:24:59 +0800

    [MISC] Add interface for IComposableStable

--- contracts/interfaces/Balancer/IComposableStable.sol
+++ contracts/interfaces/Balancer/IComposableStable.sol
@@ -10,20 +10,20 @@ interface IComposableStable is IBasePool {
         view
         returns (uint256 lastJoinExitAmplification, uint256 lastPostJoinExitInvariant);
 
-    function getAmplificationParameter()
-        external
-        view
-        returns (uint256 value, bool isUpdating, uint256 precision);
+    function getAmplificationParameter() external view returns (uint256 value, bool isUpdating, uint256 precision);
 
     function getProtocolFeePercentageCache(uint256 feeType) external view returns (uint256);
 
     function isTokenExemptFromYieldProtocolFee(IERC20 token) external view returns (bool);
 
-    function getTokenRateCache(
-        IERC20 token
-    ) external view returns (uint256 rate, uint256 oldRate, uint256 duration, uint256 expires);
+    function getTokenRateCache(IERC20 token)
+        external
+        view
+        returns (uint256 rate, uint256 oldRate, uint256 duration, uint256 expires);
 
     function getRateProviders() external view returns (IRateProvider[] memory);
 
     function getBptIndex() external view returns (uint256);
+
+    function getTokenRate(IERC20 token) external view returns (uint256);
 }
