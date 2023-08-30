64e1b2f72624a579b24e1cbccaba5b822e2a05c4
ngfam <ngfamgithub@gmail.com>
2023-08-30 16:55:09 +0700

    [MF] Lybra eUSD

---
+++ contracts/interfaces/Lybra/IEUSD.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+interface IEUSD {
+    function transferShares(address _recipient, uint256 _sharesAmount) external returns (uint256);
+
+    function getSharesByMintedEUSD(uint256 _EUSDAmount) external view returns (uint256);
+
+    function getMintedEUSDByShares(uint256 _sharesAmount) external view returns (uint256);
+
+    function sharesOf(address _account) external view returns (uint256);
+}
