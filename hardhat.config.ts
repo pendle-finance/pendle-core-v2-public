c114e2c54299b529de07470d52cab646ef08e843
UncleGrandpa925 <vuonglongtoan@gmail.com>
2023-12-18 10:28:43 +0700

    Add production compile setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,20 @@ import '@typechain/hardhat';
 import "hardhat-contract-sizer";
 import { HardhatUserConfig } from "hardhat/types";
 
+function viaIR(version: string, runs: number) {
+    return {
+        version,
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: runs,
+            },
+            evmVersion: 'paris',
+            viaIR: true,
+        },
+    };
+}
+
 const config: HardhatUserConfig = {
     paths: {
         sources: './contracts',
@@ -22,6 +36,14 @@ const config: HardhatUserConfig = {
                 },
             }
         ],
+        overrides: {
+            'contracts/router/ActionAddRemoveLiqV3.sol': viaIR('0.8.23', 10000),
+            'contracts/router/ActionMiscV3.sol': viaIR('0.8.23', 1000000),
+            'contracts/router/ActionSwapPTV3.sol': viaIR('0.8.23', 1000000),
+            'contracts/router/ActionSwapYTV3.sol': viaIR('0.8.23', 1000000),
+            'contracts/router/ActionCallbackV3.sol': viaIR('0.8.23', 1000000),
+            'contracts/router/PendleRouterV3.sol': viaIR('0.8.23', 1000000),
+        },
     },
     contractSizer: {
         disambiguatePaths: false,
