d163de34e0aec1fc5ffa6bfbe73730b3db4dd8f2
UncleGrandpa925 <vuonglongtoan@gmail.com>
2024-02-17 17:41:49 +0700

    Minor optimization for decimals in Redstone

---
+++ contracts/interfaces/IRedstonePriceFeed.sol
@@ -0,0 +1,15 @@
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+interface IRedstonePriceFeed is AggregatorV3Interface {
+    /**
+     * @notice Old Chainlink function for getting the number of latest round
+     * @return latestRound The number of the latest update round
+     */
+    function latestRound() external view returns (uint80);
+
+    /**
+     * @notice Old Chainlink function for getting the latest successfully reported value
+     * @return latestAnswer The latest successfully reported value
+     */
+    function latestAnswer() external view returns (int256);
+}
--- contracts/oracles/PendleRedStoneRateOracleAdapter.sol
+++ contracts/oracles/PendleRedStoneRateOracleAdapter.sol
@@ -3,21 +3,23 @@ pragma solidity ^0.8.17;
 
 import "../core/libraries/math/PMath.sol";
 import "../interfaces/IPExchangeRateOracle.sol";
-import {AggregatorV3Interface as IChainlinkAggregator} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
+import "../interfaces/IRedstonePriceFeed.sol";
 
 contract PendleRedStoneRateOracleAdapter is IPExchangeRateOracle {
     using PMath for int256;
 
     address public immutable oracle;
     uint8 public immutable decimals;
+    uint8 public immutable rawDecimals;
 
     constructor(address _redStoneOracle, uint8 _decimals) {
         oracle = _redStoneOracle;
         decimals = _decimals;
+        rawDecimals = IRedstonePriceFeed(_redStoneOracle).decimals();
     }
 
     function getExchangeRate() external view returns (uint256) {
-        (, int256 answer, , , ) = IChainlinkAggregator(oracle).latestRoundData();
-        return (answer.Uint() * 10 ** decimals) / 10 ** IChainlinkAggregator(oracle).decimals();
+        int256 answer = IRedstonePriceFeed(oracle).latestAnswer();
+        return (answer.Uint() * 10 ** decimals) / 10 ** rawDecimals;
     }
 }
