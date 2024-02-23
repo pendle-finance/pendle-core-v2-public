9d5083d82996ddb2f9820a1f111714fc004b9998
ngfam <ngfamgithub@gmail.com>
2024-02-23 19:03:50 +0700

    Ignore from != address(0) case for supply check

--- contracts/core/StandardizedYield/implementations/Ethena/PendleUSDESY.sol
+++ contracts/core/StandardizedYield/implementations/Ethena/PendleUSDESY.sol
@@ -24,10 +24,17 @@ contract PendleUSDESY is PendleERC20SY {
     }
 
     // @dev: whenNotPaused not needed as it has already been added to beforeTransfer
-    function _afterTokenTransfer(address, address, uint256) internal virtual override {
+    function _afterTokenTransfer(address from, address, uint256) internal virtual override {
+        // only check for minting case 
+        // saving gas on user->user transfers
+        // skip supply cap checking on burn to allow lowering supply cap
+        if (from != address(0)) {
+            return;
+        }
+
         uint256 _supply = totalSupply();
         uint256 _supplyCap = supplyCap;
-        if (_supply >= _supplyCap) {
+        if (_supply > _supplyCap) {
             revert SupplyCapExceeded(_supply, _supplyCap);
         }
     }
