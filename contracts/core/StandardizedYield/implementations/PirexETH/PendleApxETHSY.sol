660e66f02de0bd41a79d2f04aae55f94519aef96
UncleGrandpa925 <vuonglongtoan@gmail.com>
2024-03-24 13:58:30 +0800

    Soften the solidity pragma

--- contracts/core/StandardizedYield/implementations/PirexETH/PendleApxETHSY.sol
+++ contracts/core/StandardizedYield/implementations/PirexETH/PendleApxETHSY.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import "../../SYBase.sol";
 import "../../../../interfaces/PirexETH/IPirexETH.sol";
