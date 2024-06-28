2861b82fe9043c0eff20c562b7da8c50d2414744
ngfam <ngfamgithub@gmail.com>
2024-06-28 13:47:55 +0700

    Fix exchangeRate for PendleMellowVaultERC4626SYUpg

--- contracts/core/StandardizedYield/implementations/Mellow/PendleMellowVaultERC4626SYUpg.sol
+++ contracts/core/StandardizedYield/implementations/Mellow/PendleMellowVaultERC4626SYUpg.sol
@@ -68,9 +68,8 @@ contract PendleMellowVaultERC4626SYUpg is PendleMellowVaultSYBaseUpg {
     }
 
     function exchangeRate() public view virtual override returns (uint256 res) {
-        uint256 totalAssets = IERC4626(erc4626).totalAssets();
-        uint256 totalSupply = IERC4626(erc4626).totalSupply();
-        return totalAssets.divDown(totalSupply);
+        (uint256 tvl, uint256 supply) = _getMellowVaultTvl();
+        return IERC4626(erc4626).convertToAssets(tvl * PMath.ONE / supply);
     }
 
     function _previewDeposit(
