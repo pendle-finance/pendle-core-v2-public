3e9ca3818e98a5e9ae877119978c180ab8851aca
ngfam <ngfamgithub@gmail.com>
2024-07-03 11:26:05 +0700

    Fix Veda typo

--- contracts/core/StandardizedYield/implementations/EtherFi/PendleWEEthsSY.sol
+++ contracts/core/StandardizedYield/implementations/EtherFi/PendleWEEthsSY.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.23;
 
 import "../PendleERC20SYUpg.sol";
-import "../../../../interfaces/EtherFi/IVedoTeller.sol";
-import "../../../../interfaces/EtherFi/IVedoAccountant.sol";
+import "../../../../interfaces/EtherFi/IVedaTeller.sol";
+import "../../../../interfaces/EtherFi/IVedaAccountant.sol";
 
 contract PendleWEEthsSY is PendleERC20SYUpg {
     using PMath for uint256;
@@ -13,16 +13,16 @@ contract PendleWEEthsSY is PendleERC20SYUpg {
     // solhint-disable ordering
 
     address public constant weETHs = 0x917ceE801a67f933F2e6b33fC0cD1ED2d5909D88;
-    address public constant vedoTeller = 0x99dE9e5a3eC2750a6983C8732E6e795A35e7B861;
+    address public constant vedaTeller = 0x99dE9e5a3eC2750a6983C8732E6e795A35e7B861;
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant WEETH = 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee;
     address public constant EETH = 0x35fA164735182de50811E8e2E824cFb9B6118ac2;
     address public constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
 
-    address public immutable vedoAccountant;
+    address public immutable vedaAccountant;
 
     constructor() PendleERC20SYUpg(weETHs) {
-        vedoAccountant = IVedoTeller(vedoTeller).accountant();
+        vedaAccountant = IVedaTeller(vedaTeller).accountant();
     }
 
     function approveAllForTeller() external {
@@ -39,7 +39,7 @@ contract PendleWEEthsSY is PendleERC20SYUpg {
         if (tokenIn == weETHs) {
             return amountDeposited;
         }
-        return IVedoTeller(vedoTeller).bulkDeposit(tokenIn, amountDeposited, 0, address(this));
+        return IVedaTeller(vedaTeller).bulkDeposit(tokenIn, amountDeposited, 0, address(this));
     }
 
     function _previewDeposit(
@@ -49,7 +49,7 @@ contract PendleWEEthsSY is PendleERC20SYUpg {
         if (tokenIn == weETHs) {
             return amountTokenToDeposit;
         }
-        uint256 rate = IVedoAccountant(vedoAccountant).getRateInQuoteSafe(tokenIn);
+        uint256 rate = IVedaAccountant(vedaAccountant).getRateInQuoteSafe(tokenIn);
         amountSharesOut = amountTokenToDeposit.divDown(rate);
     }
 
