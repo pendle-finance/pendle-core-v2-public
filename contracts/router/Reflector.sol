9cc8cbfae20654a703b361c34838341fcc93537b
UncleGrandpa925 <vuonglongtoan@gmail.com>
2024-07-24 00:47:33 +0700

    Add support for SY in reflector

--- contracts/router/Reflector.sol
+++ contracts/router/Reflector.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.17;
 
 import "../interfaces/IPAllActionV3.sol";
 import "../interfaces/IPReflector.sol";
+import "../interfaces/IPMarket.sol";
+import "../interfaces/IStandardizedYield.sol";
 import "../core/libraries/TokenHelper.sol";
 
 contract Reflector is TokenHelper, IPReflector {
@@ -54,13 +56,33 @@ contract Reflector is TokenHelper, IPReflector {
 
             value = _scaleTokenInputAndGetValue(v5);
             newCalldata = abi.encodeWithSelector(selector, v1, v2, v3, v4, v5);
+        } else if (
+            selector == IPActionAddRemoveLiqV3.addLiquiditySingleSy.selector ||
+            selector == IPActionSwapPTV3.swapExactSyForPt.selector ||
+            selector == IPActionSwapYTV3.swapExactSyForYt.selector
+        ) {
+            (address v1, address v2, , uint256 v4, ApproxParams memory v5, LimitOrderData memory v6) = abi.decode(
+                data,
+                (address, address, uint256, uint256, ApproxParams, LimitOrderData)
+            );
+
+            newCalldata = abi.encodeWithSelector(selector, v1, v2, _scaleSyInput(v2), v4, v5, v6);
+        } else if (selector == IPActionAddRemoveLiqV3.addLiquiditySingleSyKeepYt.selector) {
+            (address v1, address v2, , uint256 v4, uint256 v5) = abi.decode(
+                data,
+                (address, address, uint256, uint256, uint256)
+            );
+
+            newCalldata = abi.encodeWithSelector(selector, v1, v2, _scaleSyInput(v2), v4, v5);
         } else {
             revert("UNSUPPORTED_SELECTOR");
         }
     }
 
     function _scaleTokenInputAndGetValue(TokenInput memory inp) internal returns (uint256 ethValue) {
-        require(inp.swapData.swapType == SwapType.NONE, "SWAP_NOT_ALLOWED"); // the exit call should already swap
+        if (inp.swapData.swapType != SwapType.NONE && inp.swapData.swapType != SwapType.ETH_WETH) {
+            require(inp.swapData.needScale, "SCALE_IS_REQUIRED");
+        }
 
         inp.netTokenIn = _selfBalance(inp.tokenIn);
 
@@ -69,6 +91,16 @@ contract Reflector is TokenHelper, IPReflector {
         } else if (!approved[inp.tokenIn]) {
             IERC20(inp.tokenIn).forceApprove(ROUTER, type(uint256).max);
             approved[inp.tokenIn] = true;
+        }
+    }
+
+    function _scaleSyInput(address market) internal returns (uint256 res) {
+        (IStandardizedYield SY, , ) = IPMarket(market).readTokens();
+        res = SY.balanceOf(address(this));
+
+        if (!approved[address(SY)]) {
+            IERC20(address(SY)).forceApprove(ROUTER, type(uint256).max);
+            approved[address(SY)] = true;
         }
     }
 }
