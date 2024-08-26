e9d93211272bcaa3f2c35cf5db161d94822370e9
Tran Quang Loc <quangloctran99@gmail.com>
2024-08-26 14:51:46 +0700

    Add amount estimation via spot price function

--- contracts/router/base/MarketApproxLib.sol
+++ contracts/router/base/MarketApproxLib.sol
@@ -552,3 +552,86 @@ library MarketApproxPtOutLib {
         if (approx.guessMin > approx.guessMax || approx.eps > PMath.ONE) revert("Internal: INVALID_APPROX_PARAMS");
     }
 }
+
+library MarketApproxEstimate {
+    using MarketMathCore for MarketState;
+    using PYIndexLib for PYIndex;
+    using PMath for uint256;
+    using PMath for int256;
+
+    enum TokenType {
+        PT,
+        YT,
+        SY
+    }
+
+    function estimateAmount(
+        MarketState memory market,
+        PYIndex index,
+        uint256 blockTime,
+        uint256 amountIn,
+        TokenType tokenIn,
+        TokenType tokenOut
+    ) internal pure returns (uint256 estimatedAmountOut) {
+        uint256 assetToPtRate = uint256(
+            MarketMathCore._getExchangeRateFromImpliedRate(market.lastLnImpliedRate, market.expiry - blockTime)
+        );
+
+        uint256 ptToAssetRate = PMath.ONE.divDown(assetToPtRate);
+        uint256 ytToAssetRate = PMath.ONE - ptToAssetRate;
+
+        uint256 exactAssetIn;
+
+        if (tokenIn == TokenType.SY) {
+            exactAssetIn = index.syToAsset(amountIn);
+        } else if (tokenIn == TokenType.PT) {
+            exactAssetIn = amountIn.mulDown(ptToAssetRate);
+        } else {
+            exactAssetIn = amountIn.mulDown(ytToAssetRate);
+        }
+
+        if (tokenOut == TokenType.SY) {
+            estimatedAmountOut = index.assetToSy(exactAssetIn);
+        } else if (tokenOut == TokenType.PT) {
+            estimatedAmountOut = exactAssetIn.divDown(ptToAssetRate);
+        } else {
+            estimatedAmountOut = exactAssetIn.divDown(ytToAssetRate);
+        }
+    }
+
+    function estimateAddLiquidity(
+        MarketState memory market,
+        PYIndex index,
+        uint256 blockTime,
+        uint256 netPtOwning,
+        uint256 netSyOwning
+    ) internal pure returns (uint256 estimatedPtToAdd, uint256 estimatedSyToAdd) {
+        // Let `pa` be `estimatedPtToAdd`, `sa` be `estimatedSyToAdd`.
+
+        // Conditions to satisfy:
+        // +) Add liquidity amounts need to be proportional to the existing
+        //    liquidity:
+        //      pa / sa = totalPt / totalSy
+        //   => pa = totalPt / totalSy * sa
+        //
+        // +) Let `syToPtRate` be the spot price between the PT and SY amount.
+        //    Conversion between exessive/missing parts need to respect the
+        //    current price:
+        //      (sa - netSyOwning) * syToPtRate = netPtOwning - pa
+        //  <=> (sa - netSyOwning) * syToPtRate = netPtOwning - totalPt / totalSy * sa
+        //  <=> (sa - netSyOwning) * syToPtRate * totalSy = netPtOwning * totalSy - totalPt * sa
+        //
+        //  Let x = syToPtRate * totalSy (x can be calculated with the function `estimateAmount` above).
+        //      (sa - netSyOwning) * x = netPtOwning * totalSy - totalPt * sa
+        //  <=> sa * x - netSyOwning * x = netPtOwning * totalSy - totalPt * sa
+        //  <=> sa * (x + totalPt) = netPtOwning * totalSy + netSyOwning * x
+
+        uint256 totalSy = market.totalSy.Uint();
+        uint256 totalPt = market.totalPt.Uint();
+        uint256 x = estimateAmount(market, index, blockTime, totalSy, TokenType.SY, TokenType.PT);
+        uint256 sa = (netPtOwning * totalSy + netSyOwning * x) / (x + totalPt);
+        uint256 pa = (totalPt * sa) / totalSy;
+
+        return (pa, sa);
+    }
+}
