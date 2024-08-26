95f769809ab118f370bf5f3173ee45623af28e56
ngfam <ngfamgithub@gmail.com>
2024-08-26 09:03:10 +0700

    Implementation for eBTC SY

---
+++ contracts/core/StandardizedYield/implementations/EtherFi/PendleEBTCSY.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.23;
+
+import "../PendleERC20SYUpg.sol";
+import "../../../../interfaces/EtherFi/IVedaTeller.sol";
+import "../../../../interfaces/EtherFi/IVedaAccountant.sol";
+
+contract PendleWEEthsSY is PendleERC20SYUpg {
+    using PMath for uint256;
+
+    // solhint-disable immutable-vars-naming
+    // solhint-disable const-name-snakecase
+    // solhint-disable ordering
+
+    address public constant eBTC = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
+    address public constant tBTC = 0x18084fbA666a33d37592fA2633fD49a74DD93a88;
+    address public constant wBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address public constant vedaTeller = 0xe19a43B1b8af6CeE71749Af2332627338B3242D1;
+
+    address public immutable vedaAccountant;
+
+    constructor() PendleERC20SYUpg(eBTC) {
+        vedaAccountant = IVedaTeller(vedaTeller).accountant();
+    }
+
+    function approveAllForTeller() external {
+        _safeApproveInf(tBTC, eBTC);
+        _safeApproveInf(wBTC, eBTC);
+    }
+
+    function _deposit(
+        address tokenIn,
+        uint256 amountDeposited
+    ) internal virtual override returns (uint256 /*amountSharesOut*/) {
+        if (tokenIn == eBTC) {
+            return amountDeposited;
+        }
+        return IVedaTeller(vedaTeller).bulkDeposit(tokenIn, amountDeposited, 0, address(this));
+    }
+
+    function _previewDeposit(
+        address tokenIn,
+        uint256 amountTokenToDeposit
+    ) internal view virtual override returns (uint256 amountSharesOut) {
+        if (tokenIn == eBTC) {
+            return amountTokenToDeposit;
+        }
+        uint256 rate = IVedaAccountant(vedaAccountant).getRateInQuoteSafe(tokenIn);
+        amountSharesOut = amountTokenToDeposit.divDown(rate);
+    }
+
+    function isValidTokenIn(address token) public pure override returns (bool) {
+        return token == eBTC || token == wBTC || token == tBTC;
+    }
+
+    function getTokensIn() public pure override returns (address[] memory res) {
+        return ArrayLib.create(eBTC, wBTC, tBTC);
+    }
+}
