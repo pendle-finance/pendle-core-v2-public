366cd71a2360030d227261fb234547bf42823cc8
Tran Quang Loc <quangloctran99@gmail.com>
2024-09-12 09:45:52 +0700

    Add function to generate default parameters

--- contracts/interfaces/IPAllActionTypeV3.sol
+++ contracts/interfaces/IPAllActionTypeV3.sol
@@ -16,6 +16,8 @@ import "./IPLimitRouter.sol";
  *******************************************************************************************************************
  */
 
+function createSwapTypeNoAggregator() pure returns (SwapData memory) {}
+
 struct TokenInput {
     // TOKEN DATA
     address tokenIn;
@@ -24,6 +26,21 @@ struct TokenInput {
     // AGGREGATOR DATA
     address pendleSwap;
     SwapData swapData;
+}
+
+/// Utility to generate `TokenInput` struct for simple swapping,
+/// from one of the SY's token in, that does not invole external aggregator.
+/// @param tokenIn SY token in. Can be obtained via `IStandardizedYield#getTokensIn`
+/// @param netTokenIn amount of token in.
+function createTokenInputSimple(address tokenIn, uint256 netTokenIn) pure returns (TokenInput memory) {
+    return
+        TokenInput({
+            tokenIn: tokenIn,
+            netTokenIn: netTokenIn,
+            tokenMintSy: tokenIn,
+            pendleSwap: address(0),
+            swapData: createSwapTypeNoAggregator()
+        });
 }
 
 struct TokenOutput {
@@ -36,6 +53,21 @@ struct TokenOutput {
     SwapData swapData;
 }
 
+/// Utility to generate `TokenOutput` struct for simple swapping,
+/// to one of the SY's token in that does not invole external aggregator.
+/// @param tokenOut SY token out. Can be obtained via `IStandardizedYield#getTokensOut`
+/// @param minTokenOut minimum
+function createTokenOutputSimple(address tokenOut, uint256 minTokenOut) pure returns (TokenOutput memory) {
+    return
+        TokenOutput({
+            tokenOut: tokenOut,
+            minTokenOut: minTokenOut,
+            tokenRedeemSy: tokenOut,
+            pendleSwap: address(0),
+            swapData: createSwapTypeNoAggregator()
+        });
+}
+
 struct LimitOrderData {
     address limitRouter;
     uint256 epsSkipMarket; // only used for swap operations, will be ignored otherwise
@@ -43,6 +75,10 @@ struct LimitOrderData {
     FillOrderParams[] flashFills;
     bytes optData;
 }
+
+/// Utility to generate `LimitOrderData` that does not involve Pendle limit
+/// order.
+function createEmptyLimitOrderData() pure returns (LimitOrderData memory) {}
 
 /// @dev For guessOffchain, this is to provide a shortcut to guessing. The offchain SDK can precalculate the exact result
 /// before the tx is sent. When the tx reaches the contract, the guessOffchain will be checked first, and if it satisfies the
@@ -60,7 +96,12 @@ struct ApproxParams {
     uint256 eps;
 }
 
-function emptyApproxParams() pure returns (ApproxParams memory) {}
+/// Utility to generate ApproxParams that will enable on-chain approximation.
+/// @notice Please refer to `./IPActionSimple.sol` for the set of simpler functions,
+/// with stripped down parameters and builtin on-chain approximation.
+function createDefaultApproxParams() pure returns (ApproxParams memory) {
+    return ApproxParams({guessMin: 0, guessMax: type(uint256).max, guessOffchain: 0, maxIteration: 256, eps: 1e15});
+}
 
 struct ExitPreExpReturnParams {
     uint256 netPtFromRemove;
