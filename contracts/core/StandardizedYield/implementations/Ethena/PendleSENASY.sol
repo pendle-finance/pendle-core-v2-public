0f647daca072f7b37b11aeec2920ba22f5dcd3cd
ngfam <ngfamgithub@gmail.com>
2024-10-09 21:38:11 +0700

    Added SENA supply cap

---
+++ contracts/core/StandardizedYield/implementations/Ethena/PendleSENASY.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import "../PendleERC4626NotRedeemableToAssetSY.sol";
+import "../../../../interfaces/IPTokenWithSupplyCap.sol";
+
+contract PendleSENASY is PendleERC4626NotRedeemableToAssetSY, IPTokenWithSupplyCap {
+
+    address public constant SENA = 0x8bE3460A480c80728a8C4D7a5D5303c85ba7B3b9;
+
+    uint256 public supplyCap;
+
+    event SupplyCapUpdated(uint256 newSupplyCap);
+    error SupplyCapExceeded(uint256 totalSupply, uint256 supplyCap);
+
+
+    constructor() PendleERC4626NotRedeemableToAssetSY("SY Staked ENA", "SY-sENA", SENA) {}
+
+    /*///////////////////////////////////////////////////////////////
+                            SUPPLY CAP UPDATE
+    //////////////////////////////////////////////////////////////*/
+
+    function updateSupplyCap(uint256 newSupplyCap) external onlyOwner {
+        _updateSupplyCap(newSupplyCap);
+    }
+
+    function _updateSupplyCap(uint256 newSupplyCap) internal {
+        supplyCap = newSupplyCap;
+        emit SupplyCapUpdated(newSupplyCap);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            SUPPLY CAP LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function _previewDeposit(
+        address tokenIn,
+        uint256 amountTokenToDeposit
+    ) internal view override returns (uint256 amountSharesOut) {
+        amountSharesOut = super._previewDeposit(tokenIn, amountTokenToDeposit);
+        uint256 _newSupply = totalSupply() + amountSharesOut;
+        uint256 _supplyCap = supplyCap;
+
+        if (_newSupply > _supplyCap) {
+            revert SupplyCapExceeded(_newSupply, _supplyCap);
+        }
+    }
+
+    // @dev: whenNotPaused not needed as it has already been added to beforeTransfer
+    function _afterTokenTransfer(address from, address, uint256) internal virtual override {
+        // only check for minting case
+        // saving gas on user->user transfers
+        // skip supply cap checking on burn to allow lowering supply cap
+        if (from != address(0)) {
+            return;
+        }
+
+        uint256 _supply = totalSupply();
+        uint256 _supplyCap = supplyCap;
+        if (_supply > _supplyCap) {
+            revert SupplyCapExceeded(_supply, _supplyCap);
+        }
+    }
+
+    function getAbsoluteSupplyCap() external view returns (uint256) {
+        return supplyCap;
+    }
+
+    function getAbsoluteTotalSupply() external view returns (uint256) {
+        return totalSupply();
+    }
+}
