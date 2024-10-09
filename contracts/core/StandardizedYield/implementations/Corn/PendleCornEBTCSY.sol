7e19b5687f0db2165c667b251fed074e0b89682a
ngfam <ngfamgithub@gmail.com>
2024-10-09 14:41:25 +0700

    Remove tBTC from eBTC corn

--- contracts/core/StandardizedYield/implementations/Corn/PendleCornEBTCSY.sol
+++ contracts/core/StandardizedYield/implementations/Corn/PendleCornEBTCSY.sol
@@ -10,7 +10,6 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
     // solhint-disable const-name-snakecase
     // solhint-disable ordering
     address public constant eBTC = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
-    address public constant tBTC = 0x18084fbA666a33d37592fA2633fD49a74DD93a88;
     address public constant wBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address public constant LBTC = 0x8236a87084f8B84306f72007F36F2618A5634494;
     address public constant vedaTeller = 0xe19a43B1b8af6CeE71749Af2332627338B3242D1;
@@ -24,7 +23,6 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
     }
 
     function initialize() external initializer {
-        _safeApproveInf(tBTC, eBTC);
         _safeApproveInf(wBTC, eBTC);
         _safeApproveInf(LBTC, eBTC);
         __CornBaseSY_init_("SY ether.fi eBTC", "SY-corn-eBTC", ONERACLE);
@@ -57,10 +55,10 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
     }
 
     function isValidTokenIn(address token) public pure override returns (bool) {
-        return token == eBTC || token == wBTC || token == tBTC || token == LBTC;
+        return token == eBTC || token == wBTC || token == LBTC;
     }
 
     function getTokensIn() public pure override returns (address[] memory res) {
-        return ArrayLib.create(wBTC, tBTC, eBTC, LBTC);
+        return ArrayLib.create(wBTC, eBTC, LBTC);
     }
 }
