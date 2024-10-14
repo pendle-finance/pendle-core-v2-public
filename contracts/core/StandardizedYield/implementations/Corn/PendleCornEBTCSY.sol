c14d92dea30cd8db001157557fe9e7d36d5794a8
ngfam <ngfamgithub@gmail.com>
2024-10-14 09:51:30 +0700

    Corn EBTC SY

--- contracts/core/StandardizedYield/implementations/Corn/PendleCornEBTCSY.sol
+++ contracts/core/StandardizedYield/implementations/Corn/PendleCornEBTCSY.sol
@@ -10,11 +10,14 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
     // solhint-disable const-name-snakecase
     // solhint-disable ordering
     address public constant eBTC = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
+    address public constant cbBTC = 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf;
     address public constant wBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address public constant LBTC = 0x8236a87084f8B84306f72007F36F2618A5634494;
-    address public constant vedaTeller = 0xe19a43B1b8af6CeE71749Af2332627338B3242D1;
-    address public constant ONERACLE = 0x1F0318B5Ab2c4084692986A2C25916Cec1195cD9;
+    address public constant vedaTeller = 0x458797A320e6313c980C2bC7D270466A6288A8bB;
+
     uint256 public constant ONE_SHARE = 10 ** 8;
+    uint256 public constant PREMIUM_SHARE_BPS = 10 ** 4;
+    address public constant ONERACLE = 0x1F0318B5Ab2c4084692986A2C25916Cec1195cD9;
 
     address public immutable vedaAccountant;
 
@@ -23,6 +26,7 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
     }
 
     function initialize() external initializer {
+        _safeApproveInf(cbBTC, eBTC);
         _safeApproveInf(wBTC, eBTC);
         _safeApproveInf(LBTC, eBTC);
         __CornBaseSY_init_("SY ether.fi eBTC", "SY-corn-eBTC", ONERACLE);
@@ -52,13 +56,16 @@ contract PendleCornEBTCSY is PendleCornBaseSYUpg {
         }
         uint256 rate = IVedaAccountant(vedaAccountant).getRateInQuoteSafe(tokenIn);
         amountSharesOut = (amountTokenToDeposit * ONE_SHARE) / rate;
+
+        IVedaTeller.Asset memory data = IVedaTeller(vedaTeller).assetData(tokenIn);
+        amountSharesOut = amountSharesOut * (PREMIUM_SHARE_BPS - data.sharePremium) / PREMIUM_SHARE_BPS;
     }
 
     function isValidTokenIn(address token) public pure override returns (bool) {
-        return token == eBTC || token == wBTC || token == LBTC;
+        return token == eBTC || token == wBTC || token == cbBTC || token == LBTC;
     }
 
     function getTokensIn() public pure override returns (address[] memory res) {
-        return ArrayLib.create(wBTC, eBTC, LBTC);
+        return ArrayLib.create(wBTC, cbBTC, eBTC, LBTC);
     }
 }
