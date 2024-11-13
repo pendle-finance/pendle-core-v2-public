92ffce5a094f622619b6a2e6811002baf782deb7
ngfam <ngfamgithub@gmail.com>
2024-11-13 16:30:44 +0700

    Base deploy preparation

--- contracts/LiquidityMining/CrossChainMsg/libraries/LayerZeroHelper.sol
+++ contracts/LiquidityMining/CrossChainMsg/libraries/LayerZeroHelper.sol
@@ -5,40 +5,46 @@ library LayerZeroHelper {
     uint256 constant EVM_ADDRESS_SIZE = 20;
 
     function _getLayerZeroChainIds(uint256 chainId) internal pure returns (uint16) {
-        if (chainId == 43113) return 10106;
         // fuji testnet
-        else if (chainId == 80001) return 10109;
+        if (chainId == 43113) return 10106;
         // mumbai testnet
-        else if (chainId == 43114) return 106;
+        else if (chainId == 80001) return 10109;
         // avax mainnet
-        else if (chainId == 42161) return 110;
+        else if (chainId == 43114) return 106;
         // arbitrum one
-        else if (chainId == 56) return 102;
+        else if (chainId == 42161) return 110;
         // binance smart chain
+        else if (chainId == 56) return 102;
+        // mainnet
         else if (chainId == 1) return 101;
         // mantle
         else if (chainId == 5000) return 181;
         // optimism
         else if (chainId == 10) return 111;
+        // base
+        else if (chainId == 8453) return 184;
         assert(false);
     }
 
     function _getOriginalChainIds(uint16 chainId) internal pure returns (uint256) {
-        if (chainId == 10106) return 43113;
         // fuji testnet
-        else if (chainId == 10109) return 80001;
+        if (chainId == 10106) return 43113;
         // mumbai testnet
-        else if (chainId == 106) return 43114;
+        else if (chainId == 10109) return 80001;
         // avax mainnet
-        else if (chainId == 110) return 42161;
+        else if (chainId == 106) return 43114;
         // arbitrum one
-        else if (chainId == 102) return 56;
+        else if (chainId == 110) return 42161;
         // binance smart chain
+        else if (chainId == 102) return 56;
+        // mainnet
         else if (chainId == 101) return 1;
         // mantle
         else if (chainId == 181) return 5000;
         // optimism
         else if (chainId == 111) return 10;
+        // base
+        else if (chainId == 184) return 8453;
         assert(false);
     }
 
