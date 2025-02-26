519f9fb0baa1a56aa386e43ed396f4e3d8ab7dcc
UncleGrandpa925 <vuonglongtoan@gmail.com>
2025-02-26 16:52:28 +0800

    Deploy Sonic

--- contracts/oracles/internal/PendleSparkLinearDiscountOracleFactory.sol
+++ contracts/oracles/internal/PendleSparkLinearDiscountOracleFactory.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {PendleSparkLinearDiscountOracle} from "./PendleSparkLinearDiscountOracle.sol";
+import "../../interfaces/IPPrincipalToken.sol";
+import "../../interfaces/IPMarket.sol";
 
 contract PendleSparkLinearDiscountOracleFactory {
-    function create(address _pt, uint256 _baseDiscountPerYear) external returns (address) {
-        return address(new PendleSparkLinearDiscountOracle(_pt, _baseDiscountPerYear));
+    function createWithPt(address pt, uint256 baseDiscountPerYear) external returns (address) {
+        return address(new PendleSparkLinearDiscountOracle(pt, baseDiscountPerYear));
+    }
+
+    function createWithMarket(address market, uint256 baseDiscountPerYear) external returns (address) {
+        (, IPPrincipalToken PT, ) = IPMarket(market).readTokens();
+        return address(new PendleSparkLinearDiscountOracle(address(PT), baseDiscountPerYear));
     }
 }
