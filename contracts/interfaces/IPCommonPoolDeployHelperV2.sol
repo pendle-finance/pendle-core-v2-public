7cc72b1e11c8164458077af40e69d0adffb43fde
ngfam <ngfamgithub@gmail.com>
2025-03-03 14:40:33 +0700

    Fix interface name

--- contracts/interfaces/IPCommonPoolDeployHelperV2.sol
+++ contracts/interfaces/IPCommonPoolDeployHelperV2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./IPPoolDeployHelperV2.sol";
 
-interface IPendleCommonPoolDeployHelperV2 is IPPoolDeployHelperV2 {
+interface IPCommonPoolDeployHelperV2 is IPPoolDeployHelperV2 {
     function deployERC4626Market(
         bytes memory constructorParams,
         PoolConfig memory config,
