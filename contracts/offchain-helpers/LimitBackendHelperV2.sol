69bc1a1e2960358b28f1841a61cef8f7d99b4a3a
Tran Quang Loc <quangloctran99@gmail.com>
2025-09-03 11:32:04 +0700

    LimitBackendHelperV2

--- contracts/offchain-helpers/LimitBackendHelperV2.sol
+++ contracts/offchain-helpers/LimitBackendHelperV2.sol
@@ -9,7 +9,7 @@ import "../interfaces/IWETH.sol";
 import "../core/libraries/Errors.sol";
 import "./BytesLib.sol";
 
-contract LimitBackendHelper is TokenHelper {
+contract LimitBackendHelperV2 is TokenHelper {
     address private immutable WNATIVE;
     address private immutable original;
 
@@ -48,62 +48,48 @@ contract LimitBackendHelper is TokenHelper {
     }
 
     /// @notice to be delegatecall from LimitRouter
-    function mintSyFromTokens(
+    function mintSyFromTokensV2(
         address YT,
         address[] calldata makers,
         address[] calldata tokens,
         uint256[] calldata amounts
-    ) external returns (uint256[] memory minted) {
+    ) external returns (uint256[] memory minted, bytes[] memory error) {
         uint256 length = makers.length;
 
         address SY = IPYieldToken(YT).SY();
         minted = new uint256[](length);
+        error = new bytes[](length);
 
         for (uint256 i = 0; i < length; i++) {
             (, bytes memory result) = original.delegatecall(
-                abi.encodeWithSignature(
-                    "mintSyFromTokenRevert(address,address,address,uint256)",
-                    makers[i],
-                    SY,
-                    tokens[i],
-                    amounts[i]
-                )
+                abi.encodeCall(this.mintSyFromTokenRevert, (makers[i], SY, tokens[i], amounts[i]))
             );
-            minted[i] = parseMinted(result);
+            (minted[i], error[i]) = parseMinted(result);
         }
     }
 
-    function parseMinted(bytes memory result) internal pure returns (uint256 minted) {
-        if (result.length < 4) return type(uint256).max;
+    function parseMinted(bytes memory result) internal pure returns (uint256 minted, bytes memory error) {
+        assert(result.length >= 4 && bytes4(result) == Errors.SimulationResults.selector);
+        (bool success, bytes memory data) = abi.decode(BytesLib.slice(result, 4, result.length - 4), (bool, bytes));
 
-        bytes4 top = bytes4(BytesLib.slice(result, 0, 4));
-        if (top == bytes4(0x1934afc8)) {
-            // selector of SimulationResults
-            (, bytes memory beta) = abi.decode(BytesLib.slice(result, 4, result.length - 4), (bool, bytes));
-            minted = abi.decode(beta, (uint256));
+        if (success) {
+            minted = abi.decode(data, (uint256));
         } else {
             minted = type(uint256).max;
+            error = data;
         }
     }
 
-    function mintSyFromTokenRevert(
-        address maker,
-        address SY,
-        address token,
-        uint256 makingAmount
-    ) public returns (uint256) {
+    function mintSyFromTokenRevert(address maker, address SY, address token, uint256 makingAmount) public {
         _transferIn(token, maker, makingAmount);
         if (token == WNATIVE && !IStandardizedYield(SY).isValidTokenIn(WNATIVE)) {
             _wrap_unwrap_ETH(WNATIVE, NATIVE, makingAmount);
             token = NATIVE;
         }
         _safeApproveInf(token, SY);
-        uint256 minted = IStandardizedYield(SY).deposit{value: token == NATIVE ? makingAmount : 0}(
-            address(this),
-            token,
-            makingAmount,
-            0
+        (bool success, bytes memory result) = SY.call{value: token == NATIVE ? makingAmount : 0}(
+            abi.encodeCall(IStandardizedYield.deposit, (address(this), token, makingAmount, 0))
         );
-        revert Errors.SimulationResults(true, abi.encode(minted));
+        revert Errors.SimulationResults(success, result);
     }
 }
